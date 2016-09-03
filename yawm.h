f49d6d6f6e40abf0974789bf3727959bbdb2c398
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2016-09-03 16:55:50 +0300

    yawm: prepare for yawmd

--- yawm.h
+++ yawm.h
@@ -82,4 +82,26 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #endif
 
+#define YAWM_BASE ".yawm"
+#define YAWM_LIST "winlist"
+#define YAWM_FIFO ".yawmd"
+
+enum reqtype {
+	REQTYPE_CLEAN,
+	REQTYPE_STORE,
+	REQTYPE_FLUSH,
+	REQTYPE_RESET,
+};
+
+struct clientinfo {
+	uint32_t win;
+	uint8_t scr;
+	uint8_t tag;
+};
+
+struct clientreq {
+	uint8_t type;
+	struct clientinfo info;
+};
+
 #endif /* YAWM_H */
