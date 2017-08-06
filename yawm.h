5c14039c72b12da9cc79eeded21c3b22fcde0035
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2017-08-06 23:10:37 +0300

    yawm: compile time defines

--- yawm.h
+++ yawm.h
@@ -10,12 +10,28 @@
 
 #include <stdint.h>
 
+#ifndef FONT1_SIZE
 #define FONT1_SIZE 10.5
-#define FONT1_NAME "Monospace"
-#define FONT2_SIZE 10.5
-#define FONT2_NAME "fontawesome"
+#endif
 
+#ifndef FONT1_NAME
+#define FONT1_NAME "Monospace"
+#endif
+
+#ifndef FONT2_SIZE
+#define FONT2_SIZE 10.5
+#endif
+
+#ifndef FONT2_NAME
+#define FONT2_NAME "fontawesome"
+#endif
+
+#ifndef YAWM_BASE
 #define YAWM_BASE ".yawm"
+#endif
+
+#ifndef YAWM_CTRL
 #define YAWM_CTRL ".control"
+#endif
 
 #endif /* YAWM_H */
