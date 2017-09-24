3aff2dc7a9897b7baf9ae2f37e23b3a4de1b8f9a
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2017-09-24 08:54:09 +0300

    dock: tweak win size and icon position

--- src/dock.c
+++ src/dock.c
@@ -14,14 +14,13 @@
 #include <poll.h>
 
 #include <xcb/xcb.h>
+#include <xcb/xcb_util.h>
 
 #include <X11/Xlib-xcb.h>
 #include <X11/Xft/Xft.h>
 
 #include "yawm.h"
 #include "misc.h"
-
-#define FONT_H_MARGIN 2
 
 static char *font_name_ = FONT2_NAME;
 static float font_size_ = FONT1_SIZE;
@@ -43,10 +42,12 @@ static const char *icon_;
 static uint8_t len_;
 static const char *cmd_;
 
-static uint16_t x_ = 2;
-static uint16_t y_ = 2;
+static uint16_t x_;
+static uint16_t y_;
 static uint16_t w_;
 static uint16_t h_;
+static uint16_t margin_;
+static XGlyphInfo ext_;
 
 static void draw(void)
 {
@@ -62,7 +63,7 @@ static void draw(void)
 static void *task(void *arg)
 {
 	system((const char *) arg);
-	dd("'%s' exited\n", (const char *) arg);
+	dd("'%s' exited", (const char *) arg);
 	return NULL;
 }
 
@@ -77,19 +78,32 @@ static void spawn(const char *cmd)
 	pthread_create(&t, NULL, task, (void *) cmd);
 }
 
+static void resize(xcb_resize_request_event_t *e)
+{
+	dd("win 0x%x (0x%x) dim %ux%u", e->window, win_, e->width, e->height);
+
+	if (h_ != e->height) {
+		h_ = e->height;
+		y_ = (h_ + ext_.y) / 2;
+	}
+}
+
 static int events(void)
 {
+	uint8_t type;
 	xcb_generic_event_t *e = xcb_wait_for_event(dpy_);
 
 	if (!e)
 		return 0;
 
-	switch (e->response_type & ~0x80) {
+	type = XCB_EVENT_RESPONSE_TYPE(e);
+
+	switch (type) {
 	case XCB_VISIBILITY_NOTIFY:
 		switch (((xcb_visibility_notify_event_t *) e)->state) {
 		case XCB_VISIBILITY_PARTIALLY_OBSCURED: /* fall through */
 		case XCB_VISIBILITY_UNOBSCURED:
-			dd("XCB_VISIBILITY_UNOBSCURED\n");
+			dd("XCB_VISIBILITY_UNOBSCURED");
 			draw();
 			break;
 		}
@@ -104,6 +118,10 @@ static int events(void)
 	case XCB_BUTTON_PRESS:
 		if (cmd_)
 			spawn(cmd_);
+		break;
+	case XCB_RESIZE_REQUEST:
+		dd("XCB_RESIZE_REQUEST");
+		resize((xcb_resize_request_event_t *) e);
 		break;
 	}
 
@@ -143,7 +161,7 @@ static XftFont *load_font(char *info, float defsize)
 		return NULL;
 	}
 
-	dd("loaded font %s size %f\n", info, size);
+	dd("loaded font %s size %.1f", info, size);
 	return font;
 }
 
@@ -156,12 +174,14 @@ static void help(const char *name)
 {
 	printf("Usage: %s <options>\n"
 	       "Options:\n"
-	       "-f, --font <font>     icon font (%s:%f)\n"
+	       "-f, --font <font>     icon font (%s:%.1f)\n"
+	       "-m, --margin <num>    icon margin (%u)\n"
+	       "-s, --size <num>      window size (auto)\n"
 	       "-i, --icon <glyph>    icon to display\n"
 	       "-c, --cmd <str>       command to run on mouse click\n"
 	       "-bg, --bgcolor <hex>  rgb color, default 0x%x\n"
 	       "-fg, --fgcolor <hex>  rgb color, default 0x%x\n\n",
-               name, font_name_, font_size_, bg_, fg_);
+               name, font_name_, font_size_, margin_, bg_, fg_);
 }
 
 int main(int argc, char *argv[])
@@ -170,10 +190,10 @@ int main(int argc, char *argv[])
 	xcb_screen_t *scr;
 	uint32_t val[2];
 	XRenderColor ref;
-	XGlyphInfo ext;
 	const char *arg;
 	const char *name;
 	uint8_t name_len;
+	uint16_t size;
 
 	if (argc < 2) {
 		help(argv[0]);
@@ -181,11 +201,16 @@ int main(int argc, char *argv[])
 	}
 
 	name = "dock";
+	size = 0; /* auto */
 
 	while (argc > 1) {
 		arg = argv[--argc];
 		if (opt(arg, "-f", "--font")) {
 			font_name_ = argv[argc + 1];
+		} else if (opt(arg, "-m", "--margin")) {
+			margin_ = atoi(argv[argc + 1]);
+		} else if (opt(arg, "-s", "--size")) {
+			size = atoi(argv[argc + 1]);
 		} else if (opt(arg, "-i", "--icon")) {
 			icon_ = argv[argc + 1];
 		} else if (opt(arg, "-c", "--cmd")) {
@@ -249,20 +274,33 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	XftTextExtentsUtf8(xdpy_, font_, (XftChar8 *) icon_, 1, &ext);
+	XftTextExtentsUtf8(xdpy_, font_, (XftChar8 *) icon_, len_, &ext_);
 
-	x_ = 2;
-	y_ = font_->ascent + font_->descent;
-	w_ = font_->max_advance_width + 2 * x_;
-	h_ = font_->ascent + font_->descent + 2 * x_;
+	x_ = margin_;
 
-	dd("win dim (%d,%d) text pos (%d,%d)\n", w_, h_, x_, y_);
+	if (size)
+		w_ = h_ = size;
+	else if (ext_.xOff > ext_.height)
+		w_ = h_ = ext_.xOff + 2 * x_;
+	else
+		w_ = h_ = ext_.height + 2 * x_;
+
+	y_ = (h_ + ext_.y) / 2;
+
+	dd("info: %ux%u%+d%+d offs %+d%+d", ext_.width, ext_.height,
+	   ext_.x, ext_.y, ext_.xOff, ext_.yOff);
+	dd("font: asc %u desc %u aw %u", font_->ascent, font_->descent,
+	   font_->max_advance_width);
+	dd("win dim (%d,%d) text pos (%d,%d) icon '%s'", w_, h_, x_, y_, icon_);
 
 	mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
 	val[0] = bg_;
-	val[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_VISIBILITY_CHANGE |
-		 XCB_EVENT_MASK_KEY_PRESS  | XCB_EVENT_MASK_BUTTON_PRESS |
-		 XCB_EVENT_MASK_POINTER_MOTION;
+	val[1] = XCB_EVENT_MASK_EXPOSURE;
+	val[1] |= XCB_EVENT_MASK_VISIBILITY_CHANGE;
+	val[1] |= XCB_EVENT_MASK_KEY_PRESS;
+	val[1] |= XCB_EVENT_MASK_BUTTON_PRESS;
+	val[1] |= XCB_EVENT_MASK_RESIZE_REDIRECT;
+
 	xcb_create_window(dpy_, XCB_COPY_FROM_PARENT, win_, scr->root,
 			  0, 0, w_, h_, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
 			  scr->root_visual, mask, val);
