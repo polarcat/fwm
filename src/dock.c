2d2602facabc8b8a471b940c2ebd26d1e75c033b
Aliaksei Katovich <aliaksei.katovich@gmail.com>
2023-07-15 23:48:52 +0300

    dock: use custom text drawing implementation

--- src/dock.c
+++ src/dock.c
@@ -5,6 +5,9 @@
  * Released under the GNU General Public License, version 2
  */
 
+#include "misc.h"
+#include "text.h"
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -16,94 +19,79 @@
 #include <xcb/xcb.h>
 #include <xcb/xcb_util.h>
 
-#include <X11/Xlib-xcb.h>
-#include <X11/Xft/Xft.h>
-
-#include "fwm.h"
-#include "misc.h"
-
+#define DEFAULT_FONT_SIZE 10.5
+#define DEFAULT_DPI 96
 #define TEXT_MAXLEN 16
 
-static char *tft_name_ = FONT1_NAME;
-static float tft_size_ = FONT1_SIZE;
-static char *ift_name_ = FONT2_NAME;
-static float ift_size_ = FONT2_SIZE;
-static uint32_t fg_ = 0x808080;
-static uint32_t bg_ = 0x202020;
+struct text_info {
+	fontid_t font_id;
+	struct text *txt;
+	char *str;
+	uint8_t len;
+	uint8_t x;
+};
 
-static int xscr_;
-static Display *xdpy_;
-static xcb_screen_t *scr_;
-static xcb_connection_t *dpy_;
-static xcb_gcontext_t gc_;
-static const char *name_ = "dock";
-static uint8_t done_;
+struct ctx {
+	xcb_connection_t *dpy;
+	xcb_screen_t *scr;
+	pid_t pid;
+	const char *name;
+	const char *cmd;
+	xcb_drawable_t win;
+	xcb_gcontext_t gc;
+	uint16_t w;
+	uint16_t h;
+	uint32_t fg;
+	uint32_t bg;
+	uint8_t done;
+	float font_size;
+	uint16_t hdpi;
+	uint16_t vdpi;
+	const char *text_font;
+	const char *icon_font;
+	uint8_t text_y;
+	uint8_t text_h;
+};
 
-static xcb_drawable_t win_;
-static uint16_t w_;
-static uint16_t h_;
+static struct ctx ctx_;
+static struct text_info text_;
+static struct text_info icon_;
 
-static XftFont *ift_;
-static XftFont *tft_;
-static XftDraw *draw_;
-static XftColor fgx_;
-
-static const char *cmd_;
-static pid_t pid_;
-
-static char *icon_;
-static xcb_rectangle_t irect_;
-static uint8_t ilen_;
-static XGlyphInfo iinfo_;
-
-static char *text_;
-static xcb_rectangle_t trect_;
-static uint8_t tlen_;
-static XGlyphInfo tinfo_;
-
-static void clean(void)
+static uint8_t update_text(struct text_info *text)
 {
-	xcb_rectangle_t rect = { 0, 0, w_, h_, };
-	xcb_change_gc(dpy_, gc_, XCB_GC_FOREGROUND, &bg_);
-	xcb_poly_fill_rectangle(dpy_, win_, gc_, 1, &rect);
+	if (!text->str)
+		return 0;
+
+	set_text_color(text->txt, ctx_.fg, ctx_.bg);
+	set_text_pos(text->txt, text->x, ctx_.text_y);
+	set_text_str(text->txt, text->str, text->len);
+	return 1;
 }
 
-static void draw(XftFont *font)
+static inline void clear(void)
 {
-	xcb_rectangle_t *rect;
-	XftChar8 *str;
-	uint8_t len;
-
-	if (font == ift_) {
-		str = (XftChar8 *) icon_;
-		len = ilen_;
-		rect = &irect_;
-	} else {
-		str = (XftChar8 *) text_;
-		len = tlen_;
-		rect = &trect_;
-	}
-
-	XftDrawStringUtf8(draw_, &fgx_, font, rect->x, rect->y, str, len);
-	xcb_flush(dpy_);
-	XSync(xdpy_, 0);
+	xcb_rectangle_t rect = { 0, 0, ctx_.w, ctx_.h, };
+	xcb_change_gc(ctx_.dpy, ctx_.gc, XCB_GC_FOREGROUND, &ctx_.bg);
+	xcb_poly_fill_rectangle(ctx_.dpy, ctx_.win, ctx_.gc, 1, &rect);
 }
 
 static void show(void)
 {
-	clean();
+	struct xcb xcb = { ctx_.dpy, ctx_.win, ctx_.gc };
 
-	if (icon_)
-		draw(ift_);
+	clear();
 
-	if (text_)
-		draw(tft_);
+	if (icon_.str && update_text(&icon_))
+		draw_text_xcb(&xcb, icon_.txt);
+
+	if (text_.str && update_text(&text_))
+		draw_text_xcb(&xcb, text_.txt);
 }
 
 static void *task(void *arg)
 {
 	system((const char *) arg);
-	dd("'%s' exited", (const char *) arg);
+	dd("'%s' exited\n", (const char *) arg);
 	return NULL;
 }
 
@@ -115,7 +103,7 @@ static void spawn(const char *cmd)
 	const char *home;
 
 	if ((arg = calloc(1, len))) {
-		snprintf(arg, len, "%s %u", cmd, pid_);
+		snprintf(arg, len, "%s %u", cmd, ctx_.pid);
 	} else {
 		ww("calloc(%zu) failed\n", len);
 		arg = (char *) cmd;
@@ -127,62 +115,14 @@ static void spawn(const char *cmd)
 	pthread_create(&t, NULL, task, (void *) arg);
 }
 
-static uint16_t h_margin(uint16_t h, XGlyphInfo *info)
-{
-	uint16_t margin;
-
-	if (!info->height)
-		return 0;
-
-	margin = h - info->height;
-
-	if (margin % 2)
-		margin++;
-
-	dd("h margin %u height %u", margin / 2, info->height);
-	return margin / 2;
-}
-
-static void adjust_rects(uint16_t w, uint16_t h)
-{
-	if (icon_ && text_) {
-		w_ = iinfo_.width + tft_size_ / 2 + tinfo_.width;
-		irect_.x = iinfo_.x;
-		irect_.width = iinfo_.width;
-		irect_.y = iinfo_.y + h_margin(h, &iinfo_);
-		irect_.height = h_;
-		trect_.x = irect_.x + tft_size_ / 2 + iinfo_.width;
-		trect_.width = tinfo_.width;
-		trect_.y = tinfo_.y + h_margin(h, &tinfo_);
-		trect_.height = h_;
-	} else if (text_) {
-		trect_.x = tinfo_.x;
-		trect_.y = tinfo_.y + h_margin(h, &tinfo_);
-		trect_.width = w_ = tinfo_.width;
-		trect_.height = h_;
-	} else { /* only icon */
-		irect_.x = iinfo_.x;
-		irect_.y = iinfo_.y + h_margin(h, &iinfo_);
-		irect_.width = w_ = iinfo_.width;
-		irect_.height = h_;
-	}
-
-	dd("i rect: %ux%u%+d%+d", irect_.width, irect_.height,
-	   irect_.x, irect_.y);
-	dd("t rect: %ux%u%+d%+d", trect_.width, trect_.height,
-	   trect_.x, trect_.y);
-	dd("win %ux%u icon '%s' pos %d,%d text '%s' pos %d,%d",
-	   w_, h_, icon_, irect_.x, irect_.y, text_, trect_.x, trect_.y);
-}
-
 static xcb_atom_t getatom(const char *str, uint8_t len)
 {
 	xcb_intern_atom_cookie_t c;
 	xcb_intern_atom_reply_t *r;
 	xcb_atom_t a;
 
-	c = xcb_intern_atom(dpy_, 0, len, str);
-	r = xcb_intern_atom_reply(dpy_, c, NULL);
+	c = xcb_intern_atom(ctx_.dpy, 0, len, str);
+	r = xcb_intern_atom_reply(ctx_.dpy, c, NULL);
 
 	if (!r) {
 		ee("xcb_intern_atom(%s) failed\n", str);
@@ -201,81 +141,70 @@ static int create_window(void)
 	uint8_t name_len;
 	xcb_atom_t atom;
 
-
-	win_ = xcb_generate_id(dpy_);
-	draw_ = XftDrawCreate(xdpy_, win_,
-			      DefaultVisual(xdpy_, xscr_),
-			      DefaultColormap(xdpy_, xscr_));
-
-	if (!draw_) {
-		ee("XftDrawCreate() failed\n");
-		return -1;
-	}
-
-	gc_ = xcb_generate_id(dpy_);
+	ctx_.win = xcb_generate_id(ctx_.dpy);
+	ctx_.gc = xcb_generate_id(ctx_.dpy);
 
 	mask = XCB_GC_FOREGROUND;
-	val[0] = fg_;
+	val[0] = ctx_.fg;
 	mask |= XCB_GC_GRAPHICS_EXPOSURES;
 	val[1] = 0;
-	xcb_create_gc(dpy_, gc_, scr_->root, mask, val);
+	xcb_create_gc(ctx_.dpy, ctx_.gc, ctx_.scr->root, mask, val);
 
 	mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-	val[0] = bg_;
+	val[0] = ctx_.bg;
 	val[1] = XCB_EVENT_MASK_EXPOSURE;
 	val[1] |= XCB_EVENT_MASK_VISIBILITY_CHANGE;
 	val[1] |= XCB_EVENT_MASK_KEY_PRESS;
 	val[1] |= XCB_EVENT_MASK_BUTTON_PRESS;
 	val[1] |= XCB_EVENT_MASK_RESIZE_REDIRECT;
 
-	xcb_create_window(dpy_, XCB_COPY_FROM_PARENT, win_, scr_->root,
-			  0, 0, w_, h_, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
-			  scr_->root_visual, mask, val);
+	xcb_create_window(ctx_.dpy, XCB_COPY_FROM_PARENT, ctx_.win,
+	 ctx_.scr->root, 0, 0, ctx_.w, ctx_.h, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+	 ctx_.scr->root_visual, mask, val);
 
-	name_len = strlen(name_);
+	name_len = strlen(ctx_.name);
 
-	xcb_change_property(dpy_, XCB_PROP_MODE_REPLACE, win_,
-			    XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
-			    name_len, name_);
+	xcb_change_property(ctx_.dpy, XCB_PROP_MODE_REPLACE, ctx_.win,
+	 XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8, name_len, ctx_.name);
 
-	xcb_change_property(dpy_, XCB_PROP_MODE_REPLACE, win_,
-			    XCB_ATOM_WM_CLASS, XCB_ATOM_STRING, 8,
-			    name_len, name_);
+	xcb_change_property(ctx_.dpy, XCB_PROP_MODE_REPLACE, ctx_.win,
+	 XCB_ATOM_WM_CLASS, XCB_ATOM_STRING, 8, name_len, ctx_.name);
 
 	atom = getatom("_NET_WM_PID", sizeof("_NET_WM_PID") - 1);
 
 	if (atom != XCB_ATOM_NONE) {
-		pid_ = getpid();
-		xcb_change_property(dpy_, XCB_PROP_MODE_REPLACE, win_, atom,
-				    XCB_ATOM_CARDINAL, 32, 1, &pid_);
-		dd("pid %u win 0x%x", pid_, win_);
+		ctx_.pid = getpid();
+		xcb_change_property(ctx_.dpy, XCB_PROP_MODE_REPLACE, ctx_.win,
+		 atom, XCB_ATOM_CARDINAL, 32, 1, &ctx_.pid);
+		dd("pid %u win 0x%x\n", ctx_.pid, ctx_.win);
 	}
 
-	xcb_map_window(dpy_, win_);
-	xcb_flush(dpy_);
+	xcb_map_window(ctx_.dpy, ctx_.win);
+	xcb_flush(ctx_.dpy);
 
+	if (ctx_.text_h < ctx_.h)
+		ctx_.text_y = (ctx_.h - ctx_.text_h) / 2;
+
+	dd("win %#x wh (%u %u)\n", ctx_.win, ctx_.w, ctx_.h);
 	return 0;
 }
 
 static void resize(xcb_resize_request_event_t *e)
 {
-	dd("win 0x%x size %ux%u --> %ux%u", e->window, w_, h_, e->width,
-	   e->height);
+	dd("win %#x wh (%u %u) --> (%u %u)\n", e->window, ctx_.w, ctx_.h,
+	 e->width, e->height);
 
-	if (e->height > h_) { /* have to re-create window to resize gc */
-		h_ = e->height;
-		XftDrawDestroy(draw_);
-		xcb_free_gc(dpy_, gc_);
-		xcb_destroy_window(dpy_, win_);
-		xcb_flush(dpy_);
+	if (e->height > ctx_.h) { /* have to re-create window to resize gc */
+		ctx_.h = e->height;
+		xcb_free_gc(ctx_.dpy, ctx_.gc);
+		xcb_destroy_window(ctx_.dpy, ctx_.win);
+		xcb_flush(ctx_.dpy);
 
 		if (create_window() < 0) {
 			ee("failed to create new window %ux%u\n", e->width, e->height);
 			exit(1); /* consider it fatal */
 		}
 	}
-
-	adjust_rects(e->width, e->height);
 }
 
 static void handle_message(xcb_client_message_event_t *e)
@@ -285,9 +214,8 @@ static void handle_message(xcb_client_message_event_t *e)
 	char *ptr = dat;
 	char *msg[3] = {0};
 	uint8_t i = 0;
-	XRenderColor ref;
 
-	dd("dat '%s' len %zu", (char *) e->data.data8, sizeof(e->data.data8));
+	dd("dat '%s' len %zu\n", (char *) e->data.data8, sizeof(e->data.data8));
 
 	msg[i] = ptr;
 
@@ -300,29 +228,23 @@ static void handle_message(xcb_client_message_event_t *e)
 		ptr++;
 	}
 
-	if (msg[0] && icon_) {
-		free(icon_);
-		icon_ = strdup(msg[0]);
-		dd("ico '%s' len %u", icon_, ilen_);
+	if (msg[0] && icon_.str) {
+		free(icon_.str);
+		icon_.str = strdup(msg[0]);
+		icon_.len = strlen(icon_.str);
+		dd("ico '%s' len %u\n", icon_.str, icon_.len);
 	}
 
 	if (msg[1]) {
-		fg_ = strtol(msg[1], NULL, 16);
-		ref.alpha = 0xffff;
-		ref.red = (fg_ & 0xff0000) >> 8;
-		ref.green = fg_ & 0xff00;
-		ref.blue = (fg_ & 0xff) << 8;
-		XftColorAllocValue(xdpy_, DefaultVisual(xdpy_, xscr_),
-				   DefaultColormap(xdpy_, xscr_), &ref,
-				   &fgx_);
-		dd("rgb '%s' len %zu", msg[1], strlen(msg[1]));
+		ctx_.fg = strtol(msg[1], NULL, 16);
+		dd("rgb '%s' len %zu\n", msg[1], strlen(msg[1]));
 	}
 
-	if (msg[2] && text_) {
-		free(text_);
-		text_ = strdup(msg[2]);
-		tlen_ = strlen(text_);
-		dd("txt '%s' len %u", text_, tlen_);
+	if (msg[2] && text_.str) {
+		free(text_.str);
+		text_.str = strdup(msg[2]);
+		text_.len = strlen(text_.str);
+		dd("txt '%s' len %u\n", text_.str, text_.len);
 	}
 
 	show();
@@ -331,7 +253,7 @@ static void handle_message(xcb_client_message_event_t *e)
 static int events(void)
 {
 	uint8_t type;
-	xcb_generic_event_t *e = xcb_wait_for_event(dpy_);
+	xcb_generic_event_t *e = xcb_wait_for_event(ctx_.dpy);
 
 	if (!e)
 		return 0;
@@ -343,96 +265,69 @@ static int events(void)
 		switch (((xcb_visibility_notify_event_t *) e)->state) {
 		case XCB_VISIBILITY_PARTIALLY_OBSCURED: /* fall through */
 		case XCB_VISIBILITY_UNOBSCURED:
-			dd("XCB_VISIBILITY_UNOBSCURED");
+			dd("XCB_VISIBILITY_UNOBSCURED\n");
 			show();
 			break;
 		}
 		break;
 	case XCB_EXPOSE:
+		dd("XCB_EXPOSE\n");
 		show();
 		break;
 	case XCB_KEY_PRESS:
 		if (((xcb_key_press_event_t *)e)->detail == 9)
-			done_ = 1;
+			ctx_.done = 1;
 		break;
 	case XCB_BUTTON_PRESS:
-		if (cmd_)
-			spawn(cmd_);
+		if (ctx_.cmd)
+			spawn(ctx_.cmd);
 		break;
 	case XCB_RESIZE_REQUEST:
-		dd("XCB_RESIZE_REQUEST");
+		dd("XCB_RESIZE_REQUEST\n");
 		resize((xcb_resize_request_event_t *) e);
 		break;
 	case XCB_CLIENT_MESSAGE:
 		handle_message((xcb_client_message_event_t *) e);
 		break;
 	default:
-		dd("got message type %d", type);
+		dd("got message type %d\n", type);
 	}
 
-	xcb_flush(dpy_);
+	xcb_flush(ctx_.dpy);
 	free(e);
 	return 1;
 }
 
-static void init_info(XGlyphInfo *info)
+static uint16_t get_space_width(fontid_t font_id)
 {
-	XftFont *font;
-	XftChar8 *str;
-	uint8_t len;
+	uint16_t w;
+	uint16_t h;
+	struct text *text = create_text();
 
-	if (info == &iinfo_) {
-		str = (XftChar8 *) icon_;
-		len = ilen_;
-		font = ift_;
-	} else {
-		str = (XftChar8 *) text_;
-		len = tlen_;
-		font = tft_;
-	}
+	if (!text)
+		return ctx_.font_size;
 
-	XftTextExtentsUtf8(xdpy_, font, (XftChar8 *) str, len, info);
-
-	dd("info: %ux%u%+d%+d offs %+d%+d", info->width, info->height,
-	   info->x, info->y, info->xOff, info->yOff);
-	dd("font: asc %u desc %u aw %u", font->ascent, font->descent,
-	   font->max_advance_width);
+	set_text_font(text, font_id, ctx_.font_size);
+	set_text_str(text, "=", sizeof("="));
+	get_text_size(text, &w, &h);
+	destroy_text(&text);
+	return w;
 }
 
-static XftFont *load_font(char *info, float defsize)
+static uint8_t init_text(struct text_info *text)
 {
-	XftFont *font;
-	float size = 0.f;
-	char *ptr = info;
-	char *end = info + strlen(info);
-	char *size_ptr = NULL;
+	uint16_t w;
 
-	while (ptr < end) {
-		if (*ptr == ':') {
-			size_ptr = ptr + 1;
-			*ptr = '\0';
-			break;
-		}
+	if (!(text->txt = create_text()))
+		return 0;
 
-		ptr++;
-	}
-
-	if (size_ptr && strlen(size_ptr))
-		size = strtof(size_ptr, NULL);
-
-	if (!size)
-		size = defsize;
-
-	font = XftFontOpen(xdpy_, xscr_, XFT_FAMILY, XftTypeString, info,
-			    XFT_SIZE, XftTypeDouble, size, NULL);
-
-	if (!font) {
-		ee("XftFontOpen(%s) failed\n", info);
-		return NULL;
-	}
-
-	dd("loaded font %s size %.1f", info, size);
-	return font;
+	set_text_font(text->txt, text->font_id, ctx_.font_size);
+	set_text_pos(text->txt, text->x, 0);
+	set_text_color(text->txt, ctx_.fg, ctx_.bg);
+	set_text_str(text->txt, text->str, text->len);
+	get_text_size(text->txt, &w, &ctx_.h);
+	ctx_.w += w;
+	return 1;
 }
 
 static int opt(const char *arg, const char *args, const char *argl)
@@ -443,105 +338,115 @@ static int opt(const char *arg, const char *args, const char *argl)
 static void help(const char *prog)
 {
 	printf("Usage: %s <options>\n"
-	       "Options:\n"
-	       "-n, --name <str>          window name (%s)\n"
-	       "-fni, --icon-font <font>  icon font (%s:%.1f)\n"
-	       "-fnt, --text-font <font>  icon font (%s:%.1f)\n"
-	       "-i, --icon <glyph>        icon to display\n"
-	       "-t, --text <str>          text next to icon (%u chars)\n"
-	       "-c, --cmd <str>           command to run on mouse click\n"
-	       "-bg, --bgcolor <hex>      rgb color, default 0x%x\n"
-	       "-fg, --fgcolor <hex>      rgb color, default 0x%x\n\n",
-               prog, name_, ift_name_, ift_size_, tft_name_, tft_size_,
-	       TEXT_MAXLEN, bg_, fg_);
+	 "\nOptions:\n"
+	 "  -n, --name <str>          window name (%s)\n"
+	 "  -i, --icon <glyph>        icon to display\n"
+	 "  -t, --text <str>          text next to icon (%u chars)\n"
+	 "  -c, --cmd <str>           command to run on mouse click\n"
+	 "  -bg, --bgcolor <hex>      rgb color, default 0x%x\n"
+	 "  -fg, --fgcolor <hex>      rgb color, default 0x%x\n"
+	 "\nEnvironment:\n"
+	 "  FWM_ICONS=%s\n"
+	 "  FWM_FONT=%s\n"
+	 "  FWM_FONT_SIZE=%f\n"
+	 "  FWM_HDPI=%u\n"
+	 "  FWM_VDPI=%u\n\n",
+	 prog, ctx_.name, TEXT_MAXLEN, ctx_.bg, ctx_.fg, ctx_.icon_font,
+	 ctx_.text_font, ctx_.font_size, ctx_.hdpi, ctx_.vdpi);
 }
 
 int main(int argc, char *argv[])
 {
-	XRenderColor ref;
+	const char *hdpi_str;
+	const char *vdpi_str;
+	const char *font_size_str;
 	const char *arg;
+
+	 /* init these defaults before checking args */
+
+	ctx_.name = "dock";
+	ctx_.fg = 0x808080;
+	ctx_.bg = 0x202020;
+
+	if ((hdpi_str = getenv("FWM_HDPI")))
+		ctx_.hdpi = atoi(hdpi_str);
+	if (ctx_.hdpi == 0)
+		ctx_.hdpi = DEFAULT_DPI;
+
+	if ((vdpi_str = getenv("FWM_VDPI")))
+		ctx_.vdpi = atoi(vdpi_str);
+	if (ctx_.vdpi == 0)
+		ctx_.vdpi = DEFAULT_DPI;
+
+	if ((font_size_str = getenv("FWM_FONT_SIZE")))
+		ctx_.font_size = strtof(font_size_str, NULL);
+	else
+		ctx_.font_size = DEFAULT_FONT_SIZE;
+
+	ctx_.icon_font = getenv("FWM_ICONS");
+	ctx_.text_font = getenv("FWM_FONT");
 
 	if (argc < 2) {
 		help(argv[0]);
-		exit(0);
+		return 0;
 	}
 
 	while (argc > 1) {
 		arg = argv[--argc];
-		if (opt(arg, "-fni", "--icon-font")) {
-			ift_name_ = argv[argc + 1];
-		} else if (opt(arg, "-fnt", "--text-font")) {
-			tft_name_ = argv[argc + 1];
-		} else if (opt(arg, "-i", "--icon")) {
-			icon_ = strdup(argv[argc + 1]);
+		if (opt(arg, "-i", "--icon")) {
+			icon_.str = strdup(argv[argc + 1]);
+			icon_.len = strlen(icon_.str);
 		} else if (opt(arg, "-t", "--text")) {
-			text_ = strdup(argv[argc + 1]);
+			text_.str = strdup(argv[argc + 1]);
+			text_.len = strlen(text_.str);
 		} else if (opt(arg, "-c", "--cmd")) {
-			cmd_ = argv[argc + 1];
+			ctx_.cmd = argv[argc + 1];
 		} else if (opt(arg, "-bg", "--bgcolor")) {
-			bg_ = strtol(argv[argc + 1], NULL, 16);
+			ctx_.bg = strtol(argv[argc + 1], NULL, 16);
 		} else if (opt(arg, "-fg", "--fgcolor")) {
-			fg_ = strtol(argv[argc + 1], NULL, 16);
+			ctx_.fg = strtol(argv[argc + 1], NULL, 16);
 		} else if (opt(arg, "-n", "--name")) {
-			name_ = argv[argc + 1];
+			ctx_.name = argv[argc + 1];
 		}
 	}
 
-	if (!icon_ && !text_) {
-		ii("missing text or icon\n");
-		help(argv[0]);
-		exit(0);
+	if (!icon_.str && !text_.str) {
+		ww("missing text or icon\n");
+		text_.str = "error";
+		text_.len = strlen(text_.str);
 	}
 
-	if (!(xdpy_ = XOpenDisplay(NULL))) {
-		ee("XOpenDisplay() failed\n");
-		return 1;
+	if (icon_.str) {
+		icon_.font_id = open_font(ctx_.icon_font, ctx_.hdpi, ctx_.vdpi);
+		if (invalid_font_id(icon_.font_id))
+			return 1;
+		else if (!init_text(&icon_))
+			return 1;
+
+		text_.x = ctx_.w;
 	}
 
-	xscr_ = DefaultScreen(xdpy_);
+	if (text_.str) {
+		text_.font_id = open_font(ctx_.text_font, ctx_.hdpi, ctx_.vdpi);
+		if (invalid_font_id(text_.font_id))
+			return 1;
+		else if (!init_text(&text_))
+			return 1;
+		else if (icon_.str) {
+			uint8_t space = get_space_width(text_.font_id);
+			text_.x += space;
+			ctx_.w += space;
+		}
+	}
+	ctx_.text_h = ctx_.h;
 
-	if (!(dpy_ = XGetXCBConnection(xdpy_))) {
+	ctx_.dpy = xcb_connect(NULL, NULL);
+	if (!ctx_.dpy) {
 		ee("xcb_connect() failed\n");
 		return 1;
 	}
 
-	if (icon_) {
-		if (!(ift_ = load_font(ift_name_, ift_size_)))
-			return 1;
-
-		ilen_ = strlen(icon_);
-		init_info(&iinfo_);
-	}
-
-	if (text_) {
-		if (!(tft_ = load_font(tft_name_, ift_size_)))
-			return 1;
-
-		tlen_ = strlen(text_);
-		init_info(&tinfo_);
-	}
-
-	if (tinfo_.width > iinfo_.width)
-		w_ = tinfo_.width;
-	else
-		w_ = iinfo_.width;
-
-	if (tinfo_.height > iinfo_.height)
-		h_ = tft_->ascent + tft_->descent;
-	else
-		h_ = ift_->ascent + ift_->descent;
-
-	adjust_rects(w_, h_);
-
-	ref.alpha = 0xffff;
-	ref.red = (fg_ & 0xff0000) >> 8;
-	ref.green = fg_ & 0xff00;
-	ref.blue = (fg_ & 0xff) << 8;
-	XftColorAllocValue(xdpy_, DefaultVisual(xdpy_, xscr_),
-			DefaultColormap(xdpy_, xscr_), &ref,
-			&fgx_);
-
-	scr_ = xcb_setup_roots_iterator(xcb_get_setup(dpy_)).data;
+	ctx_.scr = xcb_setup_roots_iterator(xcb_get_setup(ctx_.dpy)).data;
 
 	if (create_window() < 0)
 		return 1;
@@ -549,13 +454,13 @@ int main(int argc, char *argv[])
 	while (1)
 		events();
 
-	xcb_destroy_window(dpy_, win_);
-
-	if (ift_)
-		XftFontClose(xdpy_, ift_);
-
-	if (tft_)
-		XftFontClose(xdpy_, tft_);
-
+	free(icon_.str);
+	free(text_.str);
+	destroy_text(&icon_.txt);
+	destroy_text(&text_.txt);
+	xcb_destroy_window(ctx_.dpy, ctx_.win);
+	xcb_disconnect(ctx_.dpy);
+	close_font(icon_.font_id);
+	close_font(text_.font_id);
 	return 0;
 }
